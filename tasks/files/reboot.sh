d488730c16f72dbb9412c512e67d10672d9335c3
Xyn <me@xyn.ro>
2021-03-25 22:03:14 +0200

    Fixed Pi bug

--- tasks/files/reboot.sh
+++ tasks/files/reboot.sh
@@ -4,8 +4,9 @@ while :; do
         wget -q --spider http://google.com
 
         if [ $? -eq 0 ]; then
-                /usr/bin/ansible-pull -o -U https://github.com/EducaTM/educatmos.git -C master > /home/ansible/last_log
+                /usr/bin/ansible-pull -o -U https://github.com/educatm/educatmos.git -C master > /home/ansible/last_log
                 /usr/bin/python3 /home/ansible/stats/stats.py
+		[ $(uname -m) == "armv7l" ] && sudo su -c "cat /dev/null > /var/log/wtmp"
                 break
         fi
         sleep 10
